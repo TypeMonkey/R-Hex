c24a39d75d362c8e9d9f2e4b4cb7b36f4f63b4a8
Jose Guaro <gaurojose@gmail.com>
2019-08-23 17:47:25 -0700

    symbol tables work started

--- src/jg/rhex/compile/verify/StructureVerifier.java
+++ src/jg/rhex/compile/verify/StructureVerifier.java
@@ -1,14 +1,32 @@
 package jg.rhex.compile.verify;
 
-public class StructureVerifier {
+import java.util.HashMap;
+import java.util.Map;
 
-  private final TypeStore typeStore;
+import jg.rhex.common.Type;
+import jg.rhex.compile.components.structs.RStatement;
+import jg.rhex.compile.components.tnodes.TNode;
+import jg.rhex.runtime.components.GenClass;
+import jg.rhex.runtime.components.Variable;
+import jg.rhex.runtime.components.rhexspec.RhexFile;
+import jg.rhex.runtime.components.rhexspec.RhexVariable;
+
+public class StructureVerifier {
   
-  public StructureVerifier(TypeStore typeStore){
-    this.typeStore = typeStore;
+  public StructureVerifier(Map<Type, GenClass> classes, Map<String, RhexFile> rhexFiles){
+    
   }
   
-  public void verifyFunctionStructure(){
-    
+  public void verifyFileStructure(){
+    //first, check file variables and their values
+    HashMap<String, Variable> localScope = new HashMap<>();
+    for (RhexVariable variable : file.getFileVariables().values()) {
+      //at variable declaration, the variable being declared to cannot be referred to
+      TNode value = variable.getOriginal().getValue();
+      
+      
+      //at the end of evaluation, then add the variable
+      localScope.put(variable.getName(), variable);
+    }
   }
 }
--- src/jg/rhex/runtime/components/SymbolTable.java
+++ src/jg/rhex/runtime/components/SymbolTable.java
@@ -1,5 +1,100 @@
 package jg.rhex.runtime.components;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import jg.rhex.common.FunctionSignature;
+import jg.rhex.common.Type;
+import jg.rhex.runtime.components.rhexspec.RhexFile;
+
 public class SymbolTable {
 
+  private Map<String, RhexFile> rhexFileMap;
+  
+  private Map<Type, GenClass> classMap;
+  
+  private List<Map<FunctionSignature, Function>> funcMaps;
+  
+  private List<Map<String, Variable>> varMaps;
+  
+  public SymbolTable(Map<String, RhexFile> fileMap, 
+      Map<Type, GenClass> classMap, 
+      Map<String, Variable> localVars, 
+      Map<FunctionSignature, Function> localFunctions) {   
+    rhexFileMap = new HashMap<>(fileMap);
+    classMap = new HashMap<>();
+    varMaps = new ArrayList<>();
+    funcMaps = new ArrayList<>();
+    
+    varMaps.add(localVars);
+    funcMaps.add(localFunctions);
+  }
+  
+  public boolean addClass(GenClass genClass) {
+    if (classMap.containsKey(genClass.getTypeInfo())) {
+      return false;
+    }
+    return classMap.put(genClass.getTypeInfo(), genClass) == null;
+  }
+  
+  public void addFunctionMap(Map<FunctionSignature, Function> fMap) {
+    funcMaps.add(fMap);
+  }
+  
+  public boolean addLocalFunction(Function function) {
+    if (funcMaps.get(0).containsKey(function.getSignature())) {
+      return false;
+    }
+    return funcMaps.get(0).put(function.getSignature(), function) == null;
+  }
+  
+  public void setLocalFuncMap(Map<FunctionSignature, Function> funcMap) {
+    funcMaps.set(0, funcMap);
+  }
+  
+  public void addVariableMap(Map<String, Variable> varMap) {
+    varMaps.add(varMap);
+  }
+  
+  public boolean addLocalVariable(Variable variable) {
+    if (varMaps.get(0).containsKey(variable.getName())) {
+      return false;
+    }
+    return varMaps.get(0).put(variable.getName(), variable) == null;
+  }
+  
+  public void setLocalVarMap(Map<String, Variable> varMap) {
+    varMaps.set(0, varMap);
+  }
+  
+  public RhexFile findFile(String fullName) {
+    return rhexFileMap.get(fullName);
+  }
+  
+  public GenClass findClass(Type type) {
+    return classMap.get(type);
+  }
+  
+  public Function findFunction(FunctionSignature signature) {
+    for (Map<FunctionSignature, Function> map : funcMaps) {
+      Function found = map.get(signature);
+      if (found != null) {
+        return found;
+      }
+    }
+    return null;
+  }
+  
+  public Variable findVariable(String varName) {
+    for (Map<String, Variable> map : varMaps) {
+      Variable found = map.get(varName);
+      if (found != null) {
+        return found;
+      }
+    }
+    return null;
+  }
 }
